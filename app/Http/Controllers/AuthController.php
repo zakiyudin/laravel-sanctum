4294a8d46f3469eb66901ed011c2c3597fcc9644
ezkam <kf.zakiyudin@gmal.com>
2024-02-16 09:58:57 +0700

    feat(api): authentication user login & register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use App\Traits\HttpResponse;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use HttpResponse;
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        // dd(!Auth::attempt($request->only(['email', 'password'])));
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Credential doesnt match', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken("Token of" . $user->name)->plainTextToken
+        ]);
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Token of ' . $user->name)->plainTextToken
+        ]);
+    }
+
+    public function logout()
+    {
+        // 
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::resource('tasks', TaskController::class);
